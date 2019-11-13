4d7ca89ab615c17852d50c4395ca79e27e9cec85
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2019-11-13 14:56:33 -0500

    project function works assuming the removal/addition of chars at the end of the textField

--- TextTwistProject/TextTwistProject/ViewController.swift
+++ TextTwistProject/TextTwistProject/ViewController.swift
@@ -15,25 +15,24 @@ class ViewController: UIViewController {
     @IBOutlet var safeArea: UIView!
     @IBOutlet weak var nextWord: UIButton!
     
-    var word = Word.getRandomWord() // Word Data type
+    var word = Word.getRandomWord()
     lazy var wordUnscrambled = word.unscrambled
-    lazy var wordScrambled = word.scrambled //String of Word Scrambled
+    lazy var wordScrambled = word.scrambled
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         guessWord.text = wordScrambled
         userInput.delegate = self
         nextWord.isHidden = true
     }
     @IBAction func newWord(_ sender: UIButton) {
-        //get refresh of new word
-        word = Word.getRandomWord() // Word Data type
+        word = Word.getRandomWord()
         wordUnscrambled = word.unscrambled
-        wordScrambled = word.scrambled //String
+        wordScrambled = word.scrambled
         view.backgroundColor = .white
         guessWord.text = wordScrambled
         nextWord.isHidden = true
+        userInput.text = nil
     }
     
     func checkWordGuess(){
@@ -43,7 +42,6 @@ class ViewController: UIViewController {
             nextWord.isHidden = false
         } else {
             view.backgroundColor = .red
-            guessWord.text?.append("\nThe correct word is: \(wordUnscrambled)") 
         }
     }
 }
@@ -52,25 +50,22 @@ extension ViewController: UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         checkWordGuess()
-        //safeArea change color
         textField.resignFirstResponder()
-        //another function to get a new game
-        //textField.text = nil
         return true
     }
     
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
+        print("Userchar:", string)
+        
         if guessWord.text?.contains(string) ?? false{
             guessWord.text?.remove(at: (guessWord.text?.firstIndex(of: Character(string)))!)
         } else if string == ""{
-            //print("Delete detected")
-            //guessWord.text?.insert(Character(string), at: guessWord.text!.endIndex)
+            print("Delete detected")
+            guessWord.text?.insert(userInput.text?.last ?? "a", at: guessWord.text!.endIndex)
         } else {
-            //could change game label to state that
             return false
-            //guessWord.text?.insert(Character(string), at: guessWord.text!.endIndex)
         }
         return true
     }
